bb2115f2452ad8a8fec0e99612ab99fbf52c274a
Krystian Kowal <krystian@kowal.pro>
2018-07-07 16:06:52 +0200

    Add comments to gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,22 +4,35 @@ import babel from 'gulp-babel';
 import del from 'del';
 import cache from 'gulp-cached';
 import sequence from 'run-sequence';
-import colors from 'colors';
+import 'colors';
 
+/**
+ * Colored console log
+ * @param {string} message 
+ */
 function nodemonLog(message){
   console.log('[' + new Date().toString().split(' ')[4].gray + '] ' + '[nodemon] '.yellow + message.yellow);
 }
 
+/**
+ * Clean up dist/ folder
+ */
 gulp.task('clean', () => {
   return del('dist/**', {force:true});
 });
 
+/**
+ * Copy all files from /src to /dist
+ */
 gulp.task('copy', ['clean'], () => {
   return gulp
     .src(['src/**/*'])
     .pipe(gulp.dest('./dist'));
 });
 
+/**
+ * Takes all .js files, transpiles to ES5
+ */
 gulp.task('js', () => {
   return gulp.src(['src/**/*.js'])
     .pipe(cache('js'))
@@ -30,12 +43,19 @@ gulp.task('js', () => {
     .pipe(gulp.dest('dist'));
 });
 
+/**
+ * Reruns js task when file changes.
+ * Restarts nodemon
+ */
 gulp.task('watch', () => {
   gulp.watch(['src/**/*.js'], (ev) => {
     sequence('js', () => nodemon.emit('restart', `${ev.type} file: ${ev.path}`));
   });
 });
 
+/**
+ * Runs local server
+ */
 gulp.task('nodemon', ['js'], () => {
   nodemon({
     script: 'dist/app.js',
@@ -51,10 +71,16 @@ gulp.task('nodemon', ['js'], () => {
   );
 });
 
+/**
+ * Creates production ready build
+ */
 gulp.task('build', ['copy'], () => {
   gulp.start(['js']);
 });
 
+/**
+ * Default task for development
+ */
 gulp.task('default', ['build'], () => {
   gulp.start(['watch', 'nodemon']);
 });
