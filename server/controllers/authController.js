8a3472247023fc05ab1223f5b072d7ee33bb2da7
Krystian Kowal <krystian@kowal.pro>
2018-12-31 00:23:38 +0100

    Fix gh-11 and provide Google Signin insteadof  Google Plus

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,7 +5,7 @@ import validateUser from '../models/users';
 
 const GoogleStrategy = passportGoogleOauth.OAuth2Strategy;
 
-passport.serializeUser( (user, done) => {
+passport.serializeUser((user, done) => {
   done(null, user);
 });
 
@@ -16,35 +16,36 @@ passport.deserializeUser(async (userData, done) => {
   done(null, user);
 });
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL
-},
-async (accessToken, refreshToken, profile, done) => {
-  const userData = {
-    id: profile.id,
-    name: `${profile.name.givenName} ${profile.name.familyName}`,
-    email: profile.emails[0].value
-  };
-  const { error } = validateUser(userData);
-  let data = await db.query(sql.findOne, [userData.id]);
-  let user = false;
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const userData = {
+        id: profile.id,
+        name: `${profile.name.givenName} ${profile.name.familyName}`,
+        email: profile.emails[0].value
+      };
+      const { error } = validateUser(userData);
+      let data = await db.query(sql.findOne, [userData.id]);
+      let user = false;
 
-  if (data.length) {
-    if (data[0].access_privilege === 'T') {
-      user = data[0];
+      if (data.length) {
+        if (data[0].access_privilege === 'T') {
+          user = data[0];
+        }
+      } else {
+        const params = [userData.id, userData.name, userData.email];
+        await db.query(sql.create, params);
+      }
+
+      return done(error, user);
     }
-  } else {
-    const params = [
-      userData.id,
-      userData.name,
-      userData.email
-    ];
-    await db.query(sql.create, params);
-  }
-
-  return done(error, user);
-}));
+  )
+);
 
 export default passport;
